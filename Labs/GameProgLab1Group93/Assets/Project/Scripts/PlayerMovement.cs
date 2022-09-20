9143f5123836ead827f761cfeb95031341a7c1c5
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-09-21 00:08:40 +0300

    ✨: add physics material

--- Labs/GameProgLab1Group93/Assets/Project/Scripts/PlayerMovement.cs
+++ Labs/GameProgLab1Group93/Assets/Project/Scripts/PlayerMovement.cs
@@ -15,34 +15,32 @@ namespace Lab1
         private LayerMask _groundMask;
 
         private Vector2 _groundCheckPos;
-        private float _xMovement;
+        private float _colliderRadius;
+		private float _xMovement;
         private Rigidbody2D _rb;
 
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
-
-            var colliderRadius = GetComponent<CircleCollider2D>().radius;
-            _groundCheckPos = new(0, -colliderRadius);
+			_colliderRadius = GetComponent<CircleCollider2D>().radius;
 		}
 
-        private void FixedUpdate()
+		private void FixedUpdate()
         {
 			_rb.velocity = new(_xMovement * _movementSpeed, _rb.velocity.y);
 		}
 
         private bool IsGrounded()
         {
-            return Physics2D.OverlapCircle(_groundCheckPos,
-                _groundCheckRadius, _groundMask);
+            _groundCheckPos = new(gameObject.transform.position.x,
+                gameObject.transform.position.y - _colliderRadius);
+			return Physics2D.OverlapCircle(_groundCheckPos,
+				_groundCheckRadius, _groundMask);
         }
 
 		public void Move(InputAction.CallbackContext context)
         {
-            if (context.performed)
-            {
-				_xMovement = context.ReadValue<Vector2>().x;
-			}
+		    _xMovement = context.ReadValue<Vector2>().x;
 		}
 
         public void Jump(InputAction.CallbackContext context)
