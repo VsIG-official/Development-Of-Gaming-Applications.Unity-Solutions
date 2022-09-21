747b437e9d7f59694cff7320eb4f5a8e12949c29
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-09-21 12:11:06 +0300

    🎉: finish lab1

---
+++ Labs/GameProgLab1Group93/ConsoleApp1/Lab1Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab1Console
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Test test = new Test();
+			test.Print();
+
+			Test[] testArray =
+			{
+				new Test(),
+				new Test(2),
+				new Test(4),
+			};
+
+
+		}
+	}
+}
---
+++ Labs/GameProgLab1Group93/ConsoleApp1/Lab1Console/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Console
+{
+	internal class Test
+	{
+		public int MyProperty { get; set; }
+
+		public Test()
+		{
+		}
+
+		public Test(int xValue)
+		{
+			MyProperty = xValue;
+		}
+
+		public void Print()
+		{
+			Console.WriteLine(MyProperty);
+		}
+	}
+}
